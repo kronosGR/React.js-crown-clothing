5680f97404a91fc13c926a0546696bbc6d34153c
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-21 10:31:19 +0200

    Added Sign In/up page. Added signin component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.scss';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 import Header from './components/header/header.component.jsx';
 
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
         <HomePage />
       </Switch>
     </div>
