de80d724b8160d10797ee233b2a10bb1698277f6
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-21 20:31:47 +0200

    Start google authentication.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { signInWithGoogle } from '../../firebase/firebase.utils'
 
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
@@ -50,6 +51,7 @@ class SignIn extends React.Component {
           />
 
           <CustomButton type='submit'>Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle} >{' '}Sign in with Google{' '}</CustomButton>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+  apiKey: "AIzaSyATNRIHX7Alx6C9QgxYTUa4IhbMWVkiSag",
+  authDomain: "crown-db-74ea1.firebaseapp.com",
+  projectId: "crown-db-74ea1",
+  storageBucket: "crown-db-74ea1.appspot.com",
+  messagingSenderId: "131312504931",
+  appId: "1:131312504931:web:d4e706ad9b6b108134e856",
+  measurementId: "G-ZR9K68016Y"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
