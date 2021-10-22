568147e993a754403fa01fe020cd35f22c4b3033
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-22 10:28:53 +0200

    Added signup component

--- src/App.js
+++ src/App.js
@@ -35,8 +35,6 @@ class App extends React.Component {
                 id: snapshot.id,
                 ...snapshot.data(),
               },
-            }, ()=> {
-              console.log(this.state);
             });
           });
         }
