829c30d05dd4bf2de977ce2624840f7be4459757
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-22 09:58:00 +0200

    Adding user to firestore DB

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { firebase } from 'firebase/compat/app';
 
-
-const Header = ({currentUser}) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo"></Logo>
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo'></Logo>
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">SHOP</Link>
-      <Link className="option" to="/shop">CONTACT</Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
-    {
-      currentUser ?
-      <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
-      :
-      <Link className='option' to='/signin'>SIGN IN</Link>
-    }
-
   </div>
-)
+);
 
 export default Header;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,42 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const config = {
-  apiKey: "AIzaSyATNRIHX7Alx6C9QgxYTUa4IhbMWVkiSag",
-  authDomain: "crown-db-74ea1.firebaseapp.com",
-  projectId: "crown-db-74ea1",
-  storageBucket: "crown-db-74ea1.appspot.com",
-  messagingSenderId: "131312504931",
-  appId: "1:131312504931:web:d4e706ad9b6b108134e856",
-  measurementId: "G-ZR9K68016Y"
+  apiKey: 'AIzaSyATNRIHX7Alx6C9QgxYTUa4IhbMWVkiSag',
+  authDomain: 'crown-db-74ea1.firebaseapp.com',
+  projectId: 'crown-db-74ea1',
+  storageBucket: 'crown-db-74ea1.appspot.com',
+  messagingSenderId: '131312504931',
+  appId: '1:131312504931:web:d4e706ad9b6b108134e856',
+  measurementId: 'G-ZR9K68016Y',
+};
+
+export const createUserProfileDocument = async (
+  userAuth,
+  additionalData
+) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
@@ -18,7 +47,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
