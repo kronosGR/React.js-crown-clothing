0753b279b5112a801f339bdbfc0787860d971c38
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-22 10:13:53 +0200

    Set user to state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,21 +24,35 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user)
-    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(
+      async (userAuth) => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot((snapshot) => {
+            this.setState({
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data(),
+              },
+            }, ()=> {
+              console.log(this.state);
+            });
+          });
+        }
+        this.setState({ currentUser: userAuth });
+      }
+    );
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-
-
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
